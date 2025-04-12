b652468a22eb9e5a012a7f61709145160fc745c8
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-12 21:43:55 +0200

    04-fruit-cart

---
+++ 1-data-structures/04-fruit-cart.py
@@ -0,0 +1,10 @@
+favourite_fruits1 = {'🍊 orange', '🍋 lemon', '🫐 blueberries'}
+favourite_fruits2 = {'🍉 watermelon', '🍊 orange', '🍎 apple'}
+
+fruits_union = favourite_fruits1.union(favourite_fruits2)
+fruits_intersection = favourite_fruits2.intersection(favourite_fruits1)
+
+print(favourite_fruits1)
+print(favourite_fruits2)
+print(fruits_union)
+print(fruits_intersection)
