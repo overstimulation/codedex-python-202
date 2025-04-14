1bc290a31c9b9eb2a718c8b12452bb5763eb0c76
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-14 14:38:28 +0200

    12-hola

---
+++ 3-functional-programming/12-hola.py
@@ -0,0 +1,14 @@
+def translator(language):
+    translations = {
+        'spanish': {'hello': 'hola', 'goodbye': 'adiós', 'thank you': 'gracias'},
+        'french': {'hello': 'bonjour', 'goodbye': 'au revoir', 'thank you': 'merci'},
+        'italian': {'hello': 'ciao', 'goodbye': 'arrivederci', 'thank you': 'grazie'}
+    }
+
+    def translate_word(word):
+        return translations[language][word]
+    
+    return translate_word
+
+translate_to_spanish = translator('spanish')
+print(translate_to_spanish('hello'))
